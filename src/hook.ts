6ae7127126366e9e406092f24b7246d2fa0e9e6d
l1b3r_- <liber@ricardojrm.com>
2021-12-08 04:22:52 +0000

    chore: expose return type

--- src/hook.ts
+++ src/hook.ts
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 /**
  * Exported Hook
  */
-export const useHook = <T>(initialValue: T) => {
+export const useHook = <T>(
+  initialValue: T,
+): (T | Dispatch<SetStateAction<T>>)[] => {
   const [state, stateSet] = useState<T>(initialValue);
   return [state, stateSet];
 };
