38c7bc74516ae55d33e77ef0f11c300f25541e69
l1b3r_- <liber@ricardojrm.com>
2021-12-08 02:39:24 +0000

    fix: lint staged scripts

--- src/component.test.tsx
+++ src/component.test.tsx
@@ -5,7 +5,7 @@ import { Component } from './component';
 const sum = (a: number, b: number) => a + b;
 
 it('sums 5 and 2 returns 7', () => {
-  const a: number = 5;
+  const a = 5;
   expect(sum(5, 2)).toBe(7);
 });
 
