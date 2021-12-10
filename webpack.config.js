f606a7e641b57119b82506e713bc33eb7760d51e
l1b3r_- <liber@ricardojrm.com>
2021-12-10 14:03:07 +0000

    refactor: bundle using webpack 5
    BREAKING CHANGE: could break existing imports

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '___RENAME___',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[t|j]sx?$/,
+        use: 'babel-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+};
