cfcc00a3e6b3fa94bcf83f67298b9be8297195ca
l1b3r_- <liber@ricardojrm.com>
2021-12-22 20:52:16 +0000

    chore: moved export order

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+export { ExampleComponent } from './components/example';
+export type { ExampleComponentProps } from './components/example';
+
 export { useExampleHook } from './hooks/example';
 export type { ExampleHookType } from './hooks/example';
 
 export { ExampleContext, useExampleContext } from './contexts/example';
 export type { ExampleContextType } from './contexts/example';
-
-export { ExampleComponent } from './components/example';
-export type { ExampleComponentProps } from './components/example';
 
 export { ExampleProvider } from './providers/example';
 export type { ExampleProviderProps } from './providers/example';
