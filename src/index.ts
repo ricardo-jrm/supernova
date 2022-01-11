7007b2d5a245ab2d229e8ed908717f9c11187bec
l1b3r_- <liber@ricardojrm.com>
2022-01-11 13:31:30 +0000

    fix: dir name bug (replace)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { ExampleComponent } from './components/Example';
-export type { ExampleComponentProps } from './components/Example';
+export { ExampleComponent } from './components/_Example';
+export type { ExampleComponentProps } from './components/_Example';
 
 export { useExampleHook } from './hooks/useExampleHook';
 export type { ExampleHookType } from './hooks/useExampleHook';
